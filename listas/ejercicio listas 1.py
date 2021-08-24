1edb5a0c33b219fcb30c97bdf7392f378db796dc
Ruben Gomez <rubncal.95@gmail.com>
2021-08-23 20:38:11 -0500

    se modifica el ejercicio 1 de listas

--- listas/ejercicio listas 1.py
+++ listas/ejercicio listas 1.py
@@ -3,32 +3,25 @@ a: la sumatoria
 b: la productoria
 c: el mayor elemento
 d: el menor elemento"""
-
-numbers = []
+from math import prod
 
 rounds = 6
-
-higher = int(input("Enter the first number: "))
-
-numbers.append(higher)
-
-smallest = numbers[0]
-
+highest = int(input("Enter the first number: "))
+numbers = [highest]
+smallest = highest
 
 for i in range(1,rounds):
 	number = int(input(f"Enter the {i + 1}° number: "))
-
 	numbers.append(number)
 
 	if numbers[i] < smallest:
 		smallest = numbers[i]
 
-	if numbers[i] > higher:
-		higher = numbers[i] 
-
-	plus = sum(numbers)
+	if numbers[i] > highest:
+		highest = numbers[i] 	
 
 print(f"These are all your list: {numbers[:]}")
-print(f"the highest number is: {higher}")
+print(f"the highest number is: {highest}")
 print(f"the smallest number is: {smallest}")
-print(f"The plus of all numbers is: {plus}")
+print(f"The sum of all numbers is: {sum(numbers)}")
+print(f"The product is: {prod(numbers)}")
