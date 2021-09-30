9fad127f388a5dca532c17aa14a1cbb227ca97b6
Ruben Gomez <rubncal.95@gmail.com>
2021-09-30 11:19:12 -0500

    it was modified the exercise 1 of while

--- ciclo_while/ejercicio1.py
+++ ciclo_while/ejercicio1.py
@@ -1,19 +1,31 @@
 """ de los n números primos contenidos en un intérvalo (por ejemplo los números primos
 del 1 al 100) calcule la sumatoria y el promedio"""
 
-from random import randint
-from math import prod
-
-cont = 0
 list = []
-while cont < 10:
-    numbers = randint(1,100)
-    list.append(numbers)
-    print(list)
-    cont += 1
+count = 0
 
-print()
-if cont == 10:
-    print(f"The sum of all numbers is: {sum(list)}\n")
-    print(f"The product of all numbers is: {prod(list)}\n")
-    print(f"The avarage is: {sum(list) / 10}")
+def is_prime(num):
+    factors_count = 0
+
+    for x in range(1, num + 1):
+        if num % x == 0:
+            factors_count += 1
+
+    if factors_count == 2:
+        return True
+    else:
+        return False
+
+number = int(input("Enter a number: "))
+
+while is_prime(number):
+    list.append(number)
+    number = int(input("Enter other number: "))
+
+    count += 1
+
+if is_prime(number) == False:
+    print(f"\nThe number {number} isn't a prime number\n")
+
+print(f"The sum of all numbers of the list is: {sum(list)}\n")
+print(f"The avarage of all numbers of the list is: {sum(list) / count}")
