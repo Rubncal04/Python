cdd2fd8fd89bcf1c044d9d54826af7b4cdbedeca
Ruben Gomez <rubncal.95@gmail.com>
2021-10-06 10:58:34 -0500

    it was modified the exercise 3 of while, it was changed some variables and syntax

--- ciclo_while/ejercicio3.py
+++ ciclo_while/ejercicio3.py
@@ -3,26 +3,25 @@ cuántos son impares y cuántos ceros"""
 
 print("Serie Fibonacci")
 
-n_elements = [0, 1, 1, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+number = int(input("Enter, how many time do you want to count the numbers: "))
+
 even = []
 odd = []
 cero = []
+count = 1
+aux = 0
 
-ended = False
+while number > 0:
+    fbn = aux + count
+    aux = count
+    count = fbn
+    print(fbn)
+    if fbn % 2 == 0:
+        even.append(fbn)
+    else:
+        odd.append(fbn)
 
-while ended == False:
-
-    for i in n_elements:
-
-        if i == 0:
-            cero.append(i)
-        elif i % 2 == 0:
-            even.append(i)
-        else:
-            odd.append(i)
-
-    ended = True
+    number -=1
 
 print(f"\nThese are even numbers: {even}")
 print(f"These are odd numbers: {odd}")
-print(f"These are cero numbers: {cero}")
